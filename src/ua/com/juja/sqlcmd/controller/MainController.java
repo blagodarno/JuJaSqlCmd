7eafe48afe0b075f94f10f7d3c915d94565c795b
asu06 <blagodarno@gmail.com>
2016-07-13 14:49:32 +0300

    Добвалена проверка кол-ва вводимых аргументов

--- src/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/ua/com/juja/sqlcmd/controller/MainController.java
@@ -27,23 +27,40 @@ public class MainController {
         connectToDb();
     }
 
+
     private void connectToDb() {
-        view.write("Hi, user!!!");
-        while (true) {
-            view.write("Enter please: databasename|user|password");
+    view.write("Привет юзер!");
+    view.write("Введи, пожалуйста имя базы данных, имя пользователя и пароль в формате: database|userName|password");
+
+    while (true) {
+        try {
             String string = view.read();
-            String[] data = string.split("[|]");
-            String databaseName = data[0];
-            String username = data[1];
-            String password = data[2];
-            try {
-                manager.connect(databaseName, username, password);
-                break;
-            } catch (Exception e) {
-                view.write("Fail becose: " + e.getMessage() + " " + e.getCause().getMessage());
-                view.write("Try again!");
+            String[] data = string.split("\\|");
+            if (data.length != 3) {
+                throw new IllegalArgumentException("Неверно количество параметров разделенных знаком '|', ожидается 3, но есть: " + data.length);
             }
+            String databaseName = data[0];
+            String userName = data[1];
+            String password = data[2];
+
+            manager.connect(databaseName, userName, password);
+            break;
+        } catch (Exception e) {
+            printError(e);
         }
-        view.write("Success!");
     }
+
+    view.write("Успех!");
+}
+
+    private void printError(Exception e) {
+        String message = /*e.getClass().getSimpleName() + ": " + */ e.getMessage();
+        Throwable cause = e.getCause();
+        if (cause != null) {
+            message += " " + /*cause.getClass().getSimpleName() + ": " + */ cause.getMessage();
+        }
+        view.write("Неудача! по причине: " + message);
+        view.write("Повтори попытку.");
+    }
+
 }
