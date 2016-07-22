8982d24e6cc0cce84e04835ad1dff25e0e1de773
asu06 <blagodarno@gmail.com>
2016-07-22 14:55:50 +0300

    Реализована  команда "list"

---
+++ src/ua/com/juja/sqlcmd/controller/command/Exit.java
@@ -0,0 +1,26 @@
+package ua.com.juja.sqlcmd.controller.command;
+
+import ua.com.juja.sqlcmd.view.View;
+
+/**
+ * Created by asu06 on 22.07.16.
+ */
+public class Exit implements Command {
+
+    private  View view;
+
+    public Exit (View view){
+        this.view=view;
+    }
+    @Override
+    public boolean canProcess(String command) {
+        return command.equals("exit");
+    }
+
+    @Override
+    public void process(String command) {
+        view.write("Bye ! ! !");
+        System.exit(0);
+
+    }
+}
